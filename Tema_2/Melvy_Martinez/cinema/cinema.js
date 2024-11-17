244cb7b58b142a61340db3764d883645ef29dc5f
Melvy Martinez <melvy_martinez@bhdleon.com.do>
2024-11-17 08:03:44 -0400

    feat: se mueve condición del if al bucle para evitar iteraciones innecesarias. Se agregan varias pruebas en consola para validar que la selección se hace de forma consecutiva mediante esta función. No se considera que un ente externo seleccione butacas al azar.

--- Tema_2/Melvy_Martinez/cinema/cinema.js
+++ Tema_2/Melvy_Martinez/cinema/cinema.js
@@ -24,10 +24,10 @@ function setup() {
 function suggest(seats) { 
   let reserved = new Set();
 
-  for (let iRow = butacas.length - 1; iRow >= 0; iRow--) {
+  for (let iRow = butacas.length - 1; iRow >= 0 && reserved.size === 0; iRow--) {
     const availablesRow = butacas[iRow].filter(p => p.estado === false);
     
-    if (availablesRow.length >= seats && reserved.size === 0) {
+    if (availablesRow.length >= seats) {
       for (let iAva = 0; iAva < seats; iAva++) {
         availablesRow[iAva].estado = true;
         reserved.add(availablesRow[iAva]);
@@ -41,8 +41,18 @@ function suggest(seats) {
 // Inicializar la matriz
 let butacas = setup();
 
-const response = suggest(10);
-console.log('Set', response);
+// Pruebas para validar selección de asientos juntos en una fila mediante esta función. No se considera que un ente externo seleccione butacas al azar.
+const response = suggest(2);
+console.log('Set', response); // { { id: 91, estado: true }, { id: 92, estado: true } }
+
+const response1 = suggest(5);
+console.log('Set', response1); // { id: 93, estado: true }, { id: 94, estado: true }, { id: 95, estado: true }, { id: 96, estado: true }, { id: 97, estado: true }
+
+const response2 = suggest(1);
+console.log('Set', response2); // { { id: 98, estado: true } }
+
+const response3 = suggest(7);
+console.log('Set', response3); // { id: 81, estado: true }, { id: 82, estado: true }, { id: 83, estado: true }, { id: 84, estado: true }, { id: 85, estado: true }, { id: 86, estado: true }, { id: 87, estado: true }
 
 // Imprimir la matriz
-console.log(butacas);
+// console.log(butacas);
