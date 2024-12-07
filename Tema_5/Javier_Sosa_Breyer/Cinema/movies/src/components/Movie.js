39906b3038e46ff9c9b453ad1d80a2d64b53f140
javiersosabreyer <sosabreyer@gmail.com>
2024-12-07 17:35:32 +0100

    Fix card height

--- Tema_5/Javier_Sosa_Breyer/Cinema/movies/src/components/Movie.js
+++ Tema_5/Javier_Sosa_Breyer/Cinema/movies/src/components/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Movie = ({movie}) => {
     return (
-        <div className="card mb-4 rounded-3 shadow-sm">
+        <div className="card h-100 mb-4 rounded-3 shadow-sm">
             <img src={movie.thumbnail} className="card-img-top" alt={movie.title}/>
             <div className="card-body">
                 <h5 className="card-title text-warning"><b>{movie.title}</b></h5>
