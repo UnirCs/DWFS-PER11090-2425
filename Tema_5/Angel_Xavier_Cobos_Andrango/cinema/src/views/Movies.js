d11d6d1f09f942472a215e42ed65450f2ef051e2
Xavier Cobos <xavier.cobos@securitydata.net.ec>
2024-12-08 09:20:59 -0500

    Estructura pinrcipal cinema con react

---
+++ Tema_5/Angel_Xavier_Cobos_Andrango/cinema/src/views/Movies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Movies.css'; // Opcional, si necesitas estilos.
+
+const Movies = () => {
+    return (
+        <div>
+            <h1>Cine FrontEnd</h1>
+        </div>
+    );
+};
+
+export default Movies;
