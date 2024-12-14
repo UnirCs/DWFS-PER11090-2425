bee3e68f89ecac0c121d8d32422319124703badb
deibiiz <davidm.bahillo@gmail.com>
2024-12-14 19:31:09 +0100

    problemas git

--- Tema_5/David_Martínez_Bahillo/unir-react/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p> 2024 Cine unir. Todos los derechos reservados.</p>
-  </footer>
-);
-
-export default Footer;
--- Tema_5/David_Martínez_Bahillo/unir-react/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-  <header className="header">
-    <h1>Cine Unir</h1>
-  </header>
-);
-
-export default Header;
